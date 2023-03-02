dd201f6030b846796316292e1b27202d4067e8af
RuiqiGao <gao606@purdue.edu>
2023-03-02 14:09:51 -0500

    finish front end parser with parser combinator

--- src/main/scala/ImpParserCombinator.scala
+++ src/main/scala/ImpParserCombinator.scala
@@ -17,6 +17,7 @@ case object LBRACKET                  extends ImpToken
 case object RBRACKET                  extends ImpToken
 case object LBRACE                    extends ImpToken
 case object RBRACE                    extends ImpToken
+case object DEREF                     extends ImpToken
 case object IF                        extends ImpToken
 case object THEN                      extends ImpToken
 case object ELSE                      extends ImpToken
@@ -25,6 +26,7 @@ case object DO                        extends ImpToken
 case object SEMICOLON                 extends ImpToken
 case object SKIP                      extends ImpToken
 case object ABORT                     extends ImpToken
+case object ASSERT                    extends ImpToken
 
 trait ImpCompilationError
 case class ImpLexerError(msg: String) extends ImpCompilationError
@@ -52,6 +54,7 @@ object ImpLexer extends RegexParsers {
   def rb        = "]" ^^ {_ => RBRACKET}
   def lc        = "{" ^^ {_ => LBRACE}
   def rc        = "}" ^^ {_ => RBRACE}
+  def deref     = "&" ^^ {_ => DEREF}
   def _if       = "if" ^^ {_ => IF}
   def _then     = "then" ^^ {_ => THEN}
   def _else     = "else" ^^ {_ => ELSE}
@@ -60,9 +63,10 @@ object ImpLexer extends RegexParsers {
   def semi      = ";" ^^ {_ => SEMICOLON}
   def skip      = "skip" ^^ {_ => SKIP}
   def abort     = "abort" ^^ {_ => ABORT}
+  def assert    = "assert" ^^ {_ => ASSERT}
 
   def tokens: Parser[List[ImpToken]] = {
-    phrase(rep1(assign | _new | lp | rp | lb | rb | lc | rc | _if | _then | _else | _while | _do | semi | skip | abort | operation | boolean | integer | identifier))
+    phrase(rep1(assign | _new | lp | rp | lb | rb | lc | rc | _if | _then | _else | _while | _do | semi | skip | abort | assert | operation | boolean | integer | identifier | deref))
   }
 
   def apply(code: String): Either[ImpLexerError, List[ImpToken]] = {
@@ -77,12 +81,19 @@ object ImpLexer extends RegexParsers {
 
 case class ImpParserError(msg: String) extends ImpCompilationError
 
-object ImpParser extends Parsers {
+object ImpParser extends PackratParsers {
   override type Elem = ImpToken
+  case class TokenPosition(l: Int, c: Int) extends Position {
+    override def line: Int = l
+    override def column: Int = c
+    override def lineContents: String = "Token Sequence"
+    override def longString: String = lineContents + toString
+  }
   class ImpTokenReader(tokens: Seq[ImpToken]) extends Reader[ImpToken] {
     override def first: ImpToken = tokens.head
     override def atEnd: Boolean = tokens.isEmpty
-    override def pos: Position = NoPosition
+    override def pos: Position = TokenPosition(0, -tokens.size)
+    //override def pos: Position = NoPosition
     override def rest: Reader[ImpToken] = new ImpTokenReader(tokens.tail)
   }
 
@@ -105,26 +116,58 @@ object ImpParser extends Parsers {
   import Expr._
   import Stmt._
 
-  def expression: Parser[Expr] = {
-    val const_int = integer ^^ { x => Const(x.x)}
-    val const_bool = bool ^^ { b => Const(b.x)}
-    val addr = identifier ^^ {id => Addr(id.str)}
-    val atomic_exp = const_int | const_bool | addr
-    val brace_exp = (LPARENTHESIS ~ expression ~ RPARENTHESIS) ^^ {case _ ~ e ~ _ => e}
-    val elem_exp = atomic_exp | brace_exp
-    val binop = (elem_exp ~ op ~ elem_exp) ^^ {case e1 ~ op_s ~ e2 => BinOp(op_s.op, e1, e2)}
-    val fileread = (elem_exp ~ LBRACKET ~ expression ~ RBRACKET) ^^ {case e1 ~ _ ~ e2 ~ _ => FieldRead(e1, e2)}
-    // unary operation
-    binop | fileread | elem_exp //| fileread
+  lazy val var_sugar: PackratParser[Expr] = {
+    identifier ^^ {case id => FieldRead(Addr(id.str), Const(0:Int))}
   }
 
-  def program: Parser[Expr] = {
-    //expression
-    phrase(expression)
+  lazy val atomic_expr: PackratParser[Expr] = {
+    lazy val const_int = integer ^^ { x => Const(x.x)}
+    lazy val const_bool = bool ^^ { b => Const(b.x)}
+    lazy val addr = (DEREF ~ identifier) ^^ {case _ ~ id => Addr(id.str)}
+    const_int | const_bool | addr
   }
 
-  def apply(tokens: scala.collection.immutable.Seq[ImpToken]): Either[ImpParserError, Expr] = {
-    val reader = new ImpTokenReader(tokens)
+  lazy val elem_expr: PackratParser[Expr] = {
+    lazy val p_expr = (LPARENTHESIS ~ expression ~ RPARENTHESIS) ^^ {case _ ~ e ~ _ => e}
+    atomic_expr | var_sugar | p_expr
+  }
+
+  lazy val expression: PackratParser[Expr] = {
+    lazy val binop = (expression ~ op ~ expression) ^^ {case e1 ~ op_s ~ e2 => BinOp(op_s.op, e1, e2)}
+    lazy val fileread = (expression ~ LBRACKET ~ expression ~ RBRACKET) ^^ {case e1 ~ _ ~ e2 ~ _ => FieldRead(e1, e2)}
+    binop | fileread | elem_expr
+  }
+
+  lazy val statement: PackratParser[Stmt] = {
+    lazy val allocation = (identifier ~ ASSIGN ~ NEW) ^^ { case id ~ _ ~ _ => Alloc(id.str)}
+    lazy val assignment_1 = (elem_expr ~ LBRACKET ~ expression ~ RBRACKET ~ ASSIGN ~ expression) ^^ { case e1 ~ _  ~ e2 ~ _ ~ _ ~ e3 => Assign(e1, e2, e3)}
+    lazy val assignment_2 = (identifier ~ ASSIGN ~ expression) ^^ { case x ~ _  ~ e => Assign(Addr(x.str), Const(0:Int), e)}
+    lazy val conditional = (IF ~ expression ~ THEN ~ statementbody ~ ELSE ~ statementbody) ^^ { case _ ~ cond ~ _ ~ then_s ~ _ ~ else_s => Cond(cond, then_s, else_s)}
+    lazy val loop = (WHILE ~ expression ~ DO ~ statementbody) ^^ { case _ ~ e ~ _ ~ s => While(e, s)}
+    lazy val skip = SKIP ^^ { _ => Skip()}
+    lazy val abort = ABORT ^^ { _ => Abort()}
+    lazy val assert = (ASSERT ~ expression) ^^ { case _ ~ e => Cond(e, Skip(), Abort())}
+    allocation | assignment_1 | assignment_2 | conditional | loop | skip | abort | assert
+  }
+
+  lazy val tail_stmt: PackratParser[Stmt] = (statement ~ rep(SEMICOLON)) ^^ { case s ~ _ => s}
+
+  lazy val statementlist: PackratParser[Stmt] = {
+    lazy val sequence = (statement ~ rep1(SEMICOLON) ~ statementlist) ^^ { case s1 ~ _ ~ s2 => Seq(s1, s2)}
+    sequence | tail_stmt
+  }
+
+  lazy val statementbody: PackratParser[Stmt] = {
+    lazy val p_body = (LBRACE ~ statementlist ~ RBRACE) ^^ { case _ ~ s ~ _ => s}
+    p_body | statement
+  }
+
+  def program: PackratParser[Stmt] = {
+    phrase(statementlist)
+  }
+
+  def apply(tokens: scala.collection.immutable.Seq[ImpToken]): Either[ImpParserError, Stmt] = {
+    val reader = new PackratReader(new ImpTokenReader(tokens))
     program(reader) match {
       //case NoSuccess(msg, next) => Left(ImpParserError(msg))
       case Success(result, next) => Right(result)
@@ -138,7 +181,7 @@ object ImpCompiler {
   import Expr._
   import Stmt._
 
-  def apply(code: String): Either[ImpCompilationError, Expr] = {
+  def apply(code: String): Either[ImpCompilationError, Stmt] = {
     for {
       tokens <- ImpLexer(code).right
       ast <- ImpParser(tokens).right
@@ -149,14 +192,13 @@ object ImpCompiler {
 object TestSimpleParser {
   import ImpLexer._
   def main(args: Array[String]) = {
-    //parse(operation, """  >=  """) match {
-    //  case Success(matched,_) => println(matched)
-    //  case Failure(msg,_) => println("FAILURE: " + msg)
-    //  case Error(msg,_) => println("ERROR: " + msg)
-    //}
-    println(ImpLexer("""( 1 + 2 )"""))
-    println(ImpCompiler("""( 1 )"""))
-    println(ImpCompiler("""x[(1 + 2) + (3 + 2)] + 23"""))
+    val prog1 = """x := 0;
+                   if(x > 0) then { x := 1; x:= 2; } else x := 3;
+                   while (x < 10) do x:= x + 1;
+                   y := new;
+                   z := y[0];
+                   """
+    println(ImpCompiler(prog1))
   }
 }
 
